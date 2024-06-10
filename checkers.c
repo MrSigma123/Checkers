a7c76744a270ce8de2e9e90a8bbe36acdca829e3
MrSigma123 <karol.czolpinski@gmail.com>
2024-06-11 00:04:07 +0200

    Correct typos

--- checkers.c
+++ checkers.c
@@ -5,10 +5,10 @@
   
   design the game board with pawns (blue and red [for readability])
 
-  pieces mich be represented by star characters
+  pieces might be represented by star characters
   queen pieces can be distinguished by capital Q character
-  taking pieces might be notated with x
-  promotion is notated by move and Q and field name
+  capturing pieces might be notated with x
+  promotion is notated by move and Q and square name
   notation to be implemented (numerical squares notation)
 
   to enter a move the whole sequence has to be entered, so
