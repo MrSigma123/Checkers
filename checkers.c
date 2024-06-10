f10ff8a49f50fc4b3b6eaabcb2dc9a9ae2701b70
MrSigma123 <karol.czolpinski@gmail.com>
2024-06-11 00:01:23 +0200

    Create Ckeckers project and add project design overview

---
+++ checkers.c
@@ -0,0 +1,17 @@
+/* checkers.c */
+/* Project overview:
+  
+  use the grid made by myself for chess project
+  
+  design the game board with pawns (blue and red [for readability])
+
+  pieces mich be represented by star characters
+  queen pieces can be distinguished by capital Q character
+  taking pieces might be notated with x
+  promotion is notated by move and Q and field name
+  notation to be implemented (numerical squares notation)
+
+  to enter a move the whole sequence has to be entered, so
+  the input stream has to be read and interpreted by the program
+
+  */
